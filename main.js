c39803913f92903ef5d6fe5abf6762b3b39b0d29
kimon-satan <simon@simonkatan.co.uk>
2016-06-29 18:49:56 +0100

    working with textures

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ var scene = new THREE.Scene();
 var width = window.innerWidth;
 var height = window.innerHeight;
 var camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
-
+var startTime = new Date().getTime();
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+var loader = new THREE.TextureLoader();
+var material;
+
+
 
 
 var light = new THREE.AmbientLight( 0x404040 ); // soft white light
@@ -19,39 +24,45 @@ scene.add( directionalLight );
 //var geometry = new THREE.BoxGeometry( width/3, width/3,  width/3 );
 var mesh = new THREE.Object3D();
 
-var material = new THREE.MeshPhongMaterial( { color: 0x00aa00, side: THREE.DoubleSide, transparent: true, opacity: 0.5 } );
-var lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
+var geometry;
+
+//var lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
 
 //var cube = new THREE.Mesh( geometry, material );
 
-var geometry = new THREE.PlaneBufferGeometry( 200, 200, 5, 5);
-var morph = new Array(geometry.attributes.position.length);
-var orig = new Array(geometry.attributes.position.length);
-
-var p = geometry.attributes.position.array;
-
-for(var i = 0; i < morph.length; i+=3)
-{
-	orig[i] = p[i];
-	orig[i+1] = p[i+1];
-	orig[i+2] = p[i+2];
-
-	morph[i] = 0;
-	morph[i+1] = 0;
-	morph[i+2] = Math.sin((i/morph.length) * Math.PI * 2) * 50;
-}
-
-geometry.computeFaceNormals();
-
-mesh.add(new THREE.Mesh(geometry, material));
-mesh.add(new THREE.LineSegments(geometry, lineMaterial));
 
 
-scene.add(mesh);
+
+//var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+
+//geometry.computeFaceNormals();
+
+//var geometry = new THREE.PlaneBufferGeometry( 200, 200, 5, 5);
+//var morph = new Array(geometry.attributes.position.length);
+//var orig = new Array(geometry.attributes.position.length);
+
+// var p = geometry.attributes.position.array;
+
+// for(var i = 0; i < morph.length; i+=3)
+// {
+// 	orig[i] = p[i];
+// 	orig[i+1] = p[i+1];
+// 	orig[i+2] = p[i+2];
+
+// 	morph[i] = 0;
+// 	morph[i+1] = 0;
+// 	morph[i+2] = Math.sin((i/morph.length) * Math.PI * 2) * 50;
+// }
+
+
+//mesh.add(new THREE.LineSegments(geometry, lineMaterial));
+
+
+
 
 camera.position.z = 500;
 
-var startTime = new Date().getTime();
+//mesh.rotation.x += 0.5;
 
 function render() {
 	requestAnimationFrame( render );
@@ -59,17 +70,57 @@ function render() {
 
 	var ellapsedTime = new Date().getTime() - startTime;
 
-	var p = geometry.attributes.position.array;
+	// var p = geometry.attributes.position.array;
 
-	for(var i = 0; i < p.length; i++)
-	{
-		p[i] = orig[i] + morph[i] * Math.sin(ellapsedTime * 0.0005);
-	}
+	// for(var i = 0; i < p.length; i++)
+	// {
+	// 	p[i] = orig[i] + morph[i] * Math.sin(ellapsedTime * 0.005);
+	// }
 
-	geometry.attributes.position.needsUpdate = true;
+	// geometry.attributes.position.needsUpdate = true;
+	// geometry.computeFaceNormals();
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.01;
+	//mesh.rotation.x += 0.01;
+	//mesh.rotation.y += 0.01;
 	
 }
 render();
+
+// load a resource
+loader.load(
+	// resource URL
+	'skin1.jpg',
+	// Function when resource is loaded
+	function ( texture ) {
+		// do something with the texture
+		var pts = [], count = 200;
+
+
+		for ( var i = 0; i < count; i ++ ) {
+			var r = 0.5 + Math.random() * 0.5;
+			var l = 200;
+			var a = 2 * i / count * Math.PI;
+			pts.push( new THREE.Vector2 ( Math.cos( a ) * l * r, Math.sin( a ) * l * r) );
+		}
+
+		texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+		texture.repeat.set( 4/1300, 4/863 );
+		texture.offset.set(0.5,0.5);
+	
+		shape = new THREE.Shape( pts );
+		geometry = new THREE.ShapeGeometry(shape);
+
+
+		material = new THREE.MeshPhongMaterial( {  side: THREE.DoubleSide, map: texture} );
+		mesh.add(new THREE.Mesh(geometry , material));
+		scene.add(mesh);
+	},
+	// Function called when download progresses
+	function ( xhr ) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+	// Function called when download errors
+	function ( xhr ) {
+		console.log( 'An error happened' );
+	}
+);
