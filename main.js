0602da3040b7dab1a75ec99a208671e80f292320
kimon-satan <simon@simonkatan.co.uk>
2016-07-03 14:50:26 +0300

    noise movement

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+
+
 var scene = new THREE.Scene();
-//var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 var width = window.innerWidth;
 var height = window.innerHeight;
 var camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
+
 var startTime = new Date().getTime();
+var ellapsedTime = 0;
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -24,40 +29,8 @@ scene.add( directionalLight );
 //var geometry = new THREE.BoxGeometry( width/3, width/3,  width/3 );
 var mesh = new THREE.Object3D();
 
-var geometry;
-
-//var lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
-
-//var cube = new THREE.Mesh( geometry, material );
-
-
-
-
-//var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-
-//geometry.computeFaceNormals();
-
-//var geometry = new THREE.PlaneBufferGeometry( 200, 200, 5, 5);
-//var morph = new Array(geometry.attributes.position.length);
-//var orig = new Array(geometry.attributes.position.length);
-
-// var p = geometry.attributes.position.array;
-
-// for(var i = 0; i < morph.length; i+=3)
-// {
-// 	orig[i] = p[i];
-// 	orig[i+1] = p[i+1];
-// 	orig[i+2] = p[i+2];
-
-// 	morph[i] = 0;
-// 	morph[i+1] = 0;
-// 	morph[i+2] = Math.sin((i/morph.length) * Math.PI * 2) * 50;
-// }
-
-
-//mesh.add(new THREE.LineSegments(geometry, lineMaterial));
-
-
+var geometry = undefined;
+var numPoints = 20;
 
 
 camera.position.z = 500;
@@ -65,26 +38,63 @@ camera.position.z = 500;
 //mesh.rotation.x += 0.5;
 
 function render() {
-	requestAnimationFrame( render );
+	
 	renderer.render( scene, camera );
 
-	var ellapsedTime = new Date().getTime() - startTime;
+	ellapsedTime = new Date().getTime() - startTime;
 
-	// var p = geometry.attributes.position.array;
+	if(geometry)
+	{
+		var shape = new THREE.Shape();
+		updateShape(shape);
+		mesh.children[0].geometry = new THREE.ShapeGeometry(shape);
+	}
 
-	// for(var i = 0; i < p.length; i++)
-	// {
-	// 	p[i] = orig[i] + morph[i] * Math.sin(ellapsedTime * 0.005);
-	// }
 
-	// geometry.attributes.position.needsUpdate = true;
-	// geometry.computeFaceNormals();
 
-	//mesh.rotation.x += 0.01;
-	//mesh.rotation.y += 0.01;
+	requestAnimationFrame( render );
 	
 }
+
 render();
+
+function updateShape(shape) {
+
+	var verts = [];
+  	var cps_a = [];
+  	var cps_b = [];
+
+  	var a = getBlobPoint(0, numPoints, 200);
+   	var b = getBlobPoint(1,numPoints,200);
+
+
+   for(var i = 0; i < numPoints; i++)
+   {
+
+		var ab = new THREE.Vector2(); 
+		ab.subVectors(b, a);
+		ab.multiplyScalar(0.5);
+		var mid = new THREE.Vector2();
+		mid.addVectors(a, ab);
+		var flip = new THREE.Vector2();
+		flip.subVectors(a , ab);
+
+		verts.push(a);
+		cps_a.push(mid);
+		cps_b.push(flip);
+
+		a = b;
+    	b = getBlobPoint((i + 2)%numPoints,numPoints,200);
+  	}
+
+	shape.moveTo(verts[0].x, verts[0].y); 
+
+	for(var i = 0; i < numPoints; i++)
+	{
+		var ni = (i+1)%numPoints;
+		shape.bezierCurveTo(cps_a[i].x, cps_a[i].y, cps_b[ni].x , cps_b[ni].y ,verts[ni].x, verts[ni].y);
+	}
+}
 
 // load a resource
 loader.load(
@@ -93,23 +103,21 @@ loader.load(
 	// Function when resource is loaded
 	function ( texture ) {
 		// do something with the texture
-		var pts = [], count = 200;
+		var pts = [];
 
 
-		for ( var i = 0; i < count; i ++ ) {
-			var r = 0.5 + Math.random() * 0.5;
-			var l = 200;
-			var a = 2 * i / count * Math.PI;
-			pts.push( new THREE.Vector2 ( Math.cos( a ) * l * r, Math.sin( a ) * l * r) );
+		for ( var i = 0; i < numPoints; i ++ ) {
+			pts.push(getBlobPoint(i, numPoints, 200));
 		}
 
 		texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 		texture.repeat.set( 4/1300, 4/863 );
 		texture.offset.set(0.5,0.5);
 	
-		shape = new THREE.Shape( pts );
-		geometry = new THREE.ShapeGeometry(shape);
+		shape = new THREE.Shape();
+		updateShape(shape);
 
+		geometry = new THREE.ShapeGeometry(shape);
 
 		material = new THREE.MeshPhongMaterial( {  side: THREE.DoubleSide, map: texture} );
 		mesh.add(new THREE.Mesh(geometry , material));
@@ -124,3 +132,13 @@ loader.load(
 		console.log( 'An error happened' );
 	}
 );
+
+function getBlobPoint(index, numPoints, radius) {
+
+  var detune = noise.simplex2(ellapsedTime * 0.0001, index )* 0.15;
+  var x = Math.sin(index/numPoints * Math.PI * 2.0) * radius   + radius * detune;
+  var y = Math.cos(index/numPoints * Math.PI * 2.0) * radius  + radius * detune;
+
+  return new THREE.Vector2(x,y);
+
+}
