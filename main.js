e2e5e4fc9a3f44fc59c6146ae864cfba428190e2
kimon-satan <simon@simonkatan.co.uk>
2016-07-21 13:59:41 +0100

    parametricised cells

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ var uniforms = {
 	mouse:  	{value: mousePos },
 
 	scale:      {value: 2.0, gui: true, min: 1.0, max: 10.0},
+	seed:      {value: 0.01, gui: true, min: 0., max: 1., step: 0.01},
 	slices:      {value: 8.0, gui: true, min: 1.0, max: 20.0},
 	segments:      {value: 1.0, gui: true, min: 1.0, max: 10.0},
 	c_size:      {value: 0.5, gui: true, min: 0.1, max: 0.8},
@@ -50,12 +51,13 @@ var uniforms = {
 	move_add:      {value: .5, gui: true, min: 0.0, max: 1.0},
 	move_freq:      {value: 2., gui: true, min: 0.01, max: 10.0},
 	move_distort: 	{value: new THREE.Vector2(.4,2.), gui: true, min: 0.0, max: 4.0},
-	color_1:        {value: new THREE.Vector3(1.,1.,1.), gui: true, type: "color"},
-	color_2:        {value: new THREE.Vector3(1.,1.,1.), gui: true, type: "color"},
-	color_3:        {value: new THREE.Vector3(0.,0.,0.), gui: true, type: "color"},
-	colpow_1:      {value: 0.001, gui: true, min: 0.001, max: 0.5, step: 0.001},
-	colpow_2:      {value: 1., gui: true, min: 0.01, max: 3.0},
-	colpow_3:      {value: 1., gui: true, min: 0.01, max: 3.0}
+	bg_color:        {value: new THREE.Vector3(1.,1.,1.), gui: true, type: "color"},
+	fg_color:        {value: new THREE.Vector3(1.,1.,1.), gui: true, type: "color"},
+	hl_color:        {value: new THREE.Vector3(0.,0.,0.), gui: true, type: "color"},
+	fg_pow:      {value: 1., gui: true, min: 0.01, max: 3.0},
+	hl_pow:      {value: 1., gui: true, min: 0.01, max: 3.0},
+	hl_mul:      {value: 4., gui: true, min: 2., max: 15.},
+	cell_detune:      {value: .25, gui: true, min: 0., max: 1., step: 0.01}
 
 };
 
@@ -117,9 +119,11 @@ var ControlPanel = function() {
   
 };
 
-window.onload = function() {
+window.onload = function() 
+{
   var controlPanel = new ControlPanel();
   var gui = new dat.GUI();
+  gui.remember(controlPanel);
   var events = {};
   
   for (var property in uniforms) {
